56448d77d42c20bddb341888dc5f01efe535fa0a
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2018-06-04 20:47:03 +0700

    add Kill bot commands

---
+++ commandos/owner/shinde.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+
+exports.run = async (auru, message, args, prefix) => {
+    if (message.author.id !== '303011486916411392' && message.author.id !== '303011486916411392') return;
+    let embed = new RichEmbed()
+        .setColor("#ff1d00")
+        .setTitle("Bot is shutting down!")
+    await message.channel.send(embed);
+
+    process.exit(0);
+}
